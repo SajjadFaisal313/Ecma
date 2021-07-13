80c9b275f4e784fac0e89c934af932ef03cb6e4b
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-13 13:14:44 +0530

    push13july

---
+++ arrow2bigsmall.js
@@ -0,0 +1,11 @@
+// older way
+function BiggerNum(num1, num2){
+    if (num1>num2){
+    return num1;
+} else{
+    return num2;
+}
+}
+
+console.log(`Bigger Number Is ${BiggerNum(455,435)}`);
+console.log(`Bigger Number Is ${BiggerNum(56,45)}`);
