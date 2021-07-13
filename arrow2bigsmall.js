3a501030536c846819779b8c62eebc7fc789113d
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-13 13:29:54 +0530

    july13push4

--- arrow2bigsmall.js
+++ arrow2bigsmall.js
@@ -9,3 +9,9 @@ function BiggerNum(num1, num2){
 
 console.log(`Bigger Number Is ${BiggerNum(455,435)}`);
 console.log(`Bigger Number Is ${BiggerNum(56,45)}`);
+
+// Now WITH ARROW Function
+
+const BiggerNum = (num1,num2) => (num1>num2) ? num1 : num2;
+
+console.log(`Bigger Number = ${BiggerNum(53445,54535)}`);
